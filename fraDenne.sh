23e522404d48380bd4ada1444ae895f30f86ac70
Anders Ekse <andere16@uia.no>
2019-08-20 18:53:14 +0200

    tatt bort noen kommentarer

--- fraDenne.sh
+++ fraDenne.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
-#echo "fraDenne" > tilDenne.sh
-
-#declare -a IPArray
-#IPArray[0]=192.168.1.1
-#IPArray[1]=192.168.1.2
-#echo ${IPArray[@]}
-
 read -p 'IP-adresse: ' ip
-#ping $ip -c 4
 echo $ip > tilTekst.txt
 del1=$(cut -c1,2,3,4,5,6,7,8,9 tilTekst.txt)
-#del1=$(cat tilTekst.txt)
 
 for i in {1..255}
 do
